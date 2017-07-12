27d23f3588ea77466b0ad3b312ebb6c9265c10e8
SEAN MCLIMANS <seanmclimans@gmail.com>
2017-07-12 15:05:56 -0600

    books displayed

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,17 @@
-$(document).ready(function () {
-  console.log("What up everybody?!!!");
-})
+$(document).ready(function() {
+  const BOOK_URL = 'https://sm-greads-api.herokuapp.com/api/v1/books';
+  const getBooks = () => $.get(BOOK_URL)
+
+  getBooks().then(showBooks)
+
+});
+
+const showBooks = (books) => {
+  const source = $('#books-template').html()
+  const template = Handlebars.compile(source)
+  const html = template({
+    books
+  })
+  console.log(html);
+  $('#book-list-items').append(html)
+}
